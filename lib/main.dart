296ab53bd277499426f2acdec3e71e2a57375eaf
nissrinayy <rescien5@gmail.com>
2025-08-08 04:34:03 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: Image.asset(
+            'assets/logo.png', // Replace with your logo asset name
+            width: 150,
+            height: 150,
+          ),
+        ),
+      ),
+    );
+  }
+}
